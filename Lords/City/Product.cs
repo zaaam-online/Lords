69097bccded5ad99cdd63c827f1e9cd75353f7db
Ilir Sejdini <ilir.sejdini@icloud.com>
2020-10-30 18:31:42 +0100

    add product

---
+++ Lords/City/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace Lords.City
+{
+    public class Product
+    {
+        public Product()
+        {
+        }
+    }
+
+    public class ProductCategory
+    {
+        private string _name = string.Empty;
+        private int _numberOfProduct = 0;
+        private Date _dateOfPurchase;
+
+        public string Name { get => _name; set => _name = value; }
+        public int Product { get => _numberOfProduct; set => _numberOfProduct = value; }
+        public Date DateOfPerchase { get => _dateOfPurchase; set => _dateOfPurchase = value; }
+    }
+
+    public enum Season { Spring, Sommer, Autumn, Winter };
+
+    public class Date
+    {
+        private int _year = 0;
+        private Season _season;
+
+        public Season season
+        {
+            get { return _season; }
+            set { _season = value; }
+        }
+
+        public int Year { get => _year; set => _year = value; }
+
+    }
+}
